1ffa01a8d2f4d4204d815d2cdabbbed7e76ad638
Aaron (wutno) <gxtxnvidia@live.com>
2011-12-14 21:04:02 +0000

    whoops!

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
- *  http://www.gnu.org/licenses/gpl.txt
+ *  http://www.gnu.org/licenses/gpl-2.0.txt
  *
  *
  *  SQL structure (NEEDED):
